fd6638e294e91556a0fc99efa7541b3a9b50f223
Natany Caxeta <natanycaxeta@icloud.com>
2019-05-03 19:58:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-array.sort do |a, b|
-  if a == b
-    0
-  elsif a < b
-    -1
-  elsif a > b
-    1
-  end
+name_hash.sort do |a, b|
+  a <=> b
+end
 end
